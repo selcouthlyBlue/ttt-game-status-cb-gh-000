8b9ab4281d8fe05b12970b2dd48c7657bdf5cb50
Jerome Patrick Gonzalvo <jvgonzalvo@up.edu.ph>
2018-06-07 08:16:36 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ end
 def winning_combination?(board, win_combination)
   char = board[win_combination[0]]
   win_combination.each do |index|
-    if position_taken?(board, index) || square != char
+    if !position_taken?(board, index) || square != char
       return false
     end
   end
